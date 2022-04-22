5421ff5a9e9b3c3daf76b4c76b590ed8ac21c7da
Tiegris <tigristibi@gmail.com>
2022-04-22 18:20:08 +0200

    fix users

--- src/files/users.py
+++ src/files/users.py
@@ -7,13 +7,13 @@ app_root = ""
 @dataclass
 class User:
     name: str
-    pwd: list[str] = []
+    pwd: list[str]
 
 
 def hash(passwd: str) -> str:
-    pass
+    return passwd
 
-def authenticate(name: str, passwd: str) -> Union(User, None):
+def authenticate(name: str, passwd: str) -> Union[User, None]:
     os_path = f'{app_root}/users'
     with open(os_path, "r") as f:
         while True:
@@ -21,7 +21,7 @@ def authenticate(name: str, passwd: str) -> Union(User, None):
             split = line.split(':', 1)
             if split[0] == name:
                 if split[1] == hash(passwd):
-                    return User(name=name)
+                    return User(name=name, pwd=[])
             if not line:
                 break
     
