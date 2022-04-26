47dbe5b4a2907af802928374595498b71768d22b
Tiegris <tigristibi@gmail.com>
2022-04-26 10:47:25 +0200

    user management

--- src/files/users.py
+++ src/files/users.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
 from typing import Tuple, Union
 from os.path import exists
+import hashlib
+import uuid
+from os import makedirs
 
 app_root = ""
+
+# import users
+# users.app_root = 'D:/pyc'
+# users.__create_home()
+# users.add_user('alice', 'aaa')
+# users.add_user('bob', 'bbb')
+# users.add_user('charlie', 'ccc')
 
 @dataclass
 class User:
@@ -10,20 +20,37 @@ class User:
     pwd: list[str]
 
 
-def hash(passwd: str) -> str:
-    return passwd
+def __create_home() -> None:
+    os_path = f'{app_root}'
+    if not exists(os_path):
+        makedirs(os_path)
+
+def __check_password(hash: str, passwd: str, salt: str) -> str:
+    return hash == hashlib.sha512(passwd.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+# Does not check if user already exists
+def add_user(user: str, passwd: str) -> None:
+    salt = uuid.uuid4().hex
+    hash = hashlib.sha512(passwd.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+    os_path = f'{app_root}/users'
+    with open(os_path, "a") as f:
+        f.write(f'{user}${salt}${hash}\n')
 
 def authenticate(name: str, passwd: str) -> Union[User, None]:
     os_path = f'{app_root}/users'
     with open(os_path, "r") as f:
         while True:
-            line = f.readline()
-            split = line.split(':', 1)
+            line = f.readline().rstrip()
+            split = line.split('$', 2)
             if split[0] == name:
-                if split[1] == hash(passwd):
+                if __check_password(split[2], passwd, split[1]):
                     return User(name=name, pwd=[])
             if not line:
                 break
-    
 
 
+app_root = 'D:/pyc'
+add_user('alice', 'aaa')
+add_user('bob', 'bbb')
+add_user('charlie', 'ccc')
+authenticate('alice', 'aaa')
