b079d31d9e473a383df5aae1ad4236cbbf7f7b99
Marton Orova <morova@rapidminer.com>
2022-05-03 13:45:07 +0200

    Read socket based on header length field

--- src/session.py
+++ src/session.py
@@ -25,14 +25,20 @@ class Session(object):
         logger.debug(f"Sent Message: {message}")
 
     def receive(self) -> Tuple[MessageType, bytes]:
-        data = self.socket.recv(2048) # messages do not exceed 1kB + MTP overhead
-        if len(data) == 0:
+        buffer: bytes = b''
+        # read in the header
+        buffer += self.socket.recv(HDR_LEN) # messages do not exceed 1kB + MTP overhead
+        if len(buffer) == 0: # empty buffer indicates connection closing from other side
             raise Exception("Read empty data from socket")
-        # if data: # on connection, data is empty --> ignore it
-        message = Message.deserialize(data)
+
+        header = Header.deserialize(buffer)
+        # read in the rest of the message
+        buffer += self.socket.recv(header.length - HDR_LEN)
+
+        message = Message.deserialize(buffer)
         logger.debug(f"Received Message: {message}")
         message_type, payload = self.decrypt(message)
-        logger.debug(f"Received payload: {payload.decode('UTF-8')}")
+        logger.debug(f"Received payload:\n{payload.decode('UTF-8')}")
 
         return message_type, payload
 
